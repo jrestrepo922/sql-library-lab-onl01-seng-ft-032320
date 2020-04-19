b1297ca82ecb555d51fc2bc74b3ad4d7c4ca1b15
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-19 13:01:42 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+
+INSERT INTO series (title, author_id, subgenre_id) VALUES ("Harry Potter", 1, 1);
+INSERT INTO series (title, author_id, subgenre_id) VALUES ("Lord of the Rings", 2, 2);
+
+
+INSERT INTO subgenres (name) VALUES ("Wizardry");
+INSERT INTO subgenres (name) VALUES ("Medieval Lore");
+
+
+INSERT INTO authors (name) VALUES ("J.K. Rowling");
+INSERT INTO authors (name) VALUES ("J.R.R. Tolkien");
