fed0072abcf4fe6b575af9bbe0aa278c9d8c53a9
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-19 13:03:09 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -9,3 +9,10 @@ INSERT INTO subgenres (name) VALUES ("Medieval Lore");
 
 INSERT INTO authors (name) VALUES ("J.K. Rowling");
 INSERT INTO authors (name) VALUES ("J.R.R. Tolkien");
+
+INSERT INTO books (title, year, series_id) VALUES ("Lord of the Rings: The Fellowship of the Ring", 1954, 2);
+INSERT INTO books (title, year, series_id) VALUES ("Lord of the Rings: The Two Towers", 1954, 2);
+INSERT INTO books (title, year, series_id) VALUES ("Lord of the Rings: The Return of the King", 1955, 2);
+INSERT INTO books (title, year, series_id) VALUES ("Harry Potter and the Sorcerer's Stone", 1997, 1);
+INSERT INTO books (title, year, series_id) VALUES ("Harry Potter and the Chamber of Secrets", 1998, 1);
+INSERT INTO books (title, year, series_id) VALUES ("Harry Potter and the Prisoner of Azkaban", 1999, 1);
